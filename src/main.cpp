02e213a7a21cc8d8489d68aa409b4b543c68bf08
Robert Hecker <robert.hecker@stud.h-da.de>
2024-09-01 15:58:29 +0200

    Allow for connecting multiple sensors, store their IDs and add functionality to add and reset them

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
+#include <OneButton.h>
+#include <EEPROM.h>
 
 const int RECEIVER_PIN = 32;
+const int BUTTON_PIN = 27;
 
 size_t currentSignalIndex = 0;
 const size_t MAX_GAPS_COUNT = 256;
@@ -16,15 +19,60 @@ const int LOW_GAP_DURATION = 2000;
 uint8_t dataBitIndex = 0;
 // The number of data bits expected in each payload.
 const size_t PAYLOAD_BITS_COUNT = 40;
-uint8_t dataBytes[PAYLOAD_BITS_COUNT / 8] = { 0 };
+const size_t PAYLOAD_BYTES_COUNT = PAYLOAD_BITS_COUNT / 8;
+uint8_t dataBytes[PAYLOAD_BYTES_COUNT];
 
-enum State {
-  SEARCHING_PREFIX, FOUND_PREFIX
+enum State
+{
+  SEARCHING_PREFIX,
+  FOUND_PREFIX,
 };
 State currentState = SEARCHING_PREFIX;
 
-unsigned long lastFullTransmissionMillis = 0;
-const unsigned long MIN_DELAY_BETWEEN_TRANSMISSIONS = 20000;
+OneButton button = OneButton(
+    BUTTON_PIN,
+    true,
+    true);
+
+// The number of weather sensors you want to connect to this program.
+const size_t NUM_SENSORS = 2;
+uint8_t sensorIds[NUM_SENSORS];
+size_t currentSensorIndex = 0;
+
+size_t EEPROM_SENSOR_IDS_ADDRESS = 0x0;
+const uint8_t EEPROM_EMPTY_VALUE = 0xFF;
+
+// Stores the payload of the last transmission per sensor.
+// This could also be defined as a nested 2-dimensional array, but for simplicity sake this works too.
+uint8_t lastDataPerSensor[PAYLOAD_BYTES_COUNT * NUM_SENSORS];
+
+bool isKnownSensor(uint8_t sensorId);
+bool isSearchingForSensors();
+void writeCurrentSensorsToStorage();
+
+bool isKnownSensor(uint8_t sensorId)
+{
+  for (size_t i = 0; i < currentSensorIndex; i++)
+  {
+    if (sensorIds[i] == sensorId)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isSearchingForSensors()
+{
+  return currentSensorIndex < NUM_SENSORS;
+}
+
+void writeCurrentSensorsToStorage() {
+  for(size_t i = 0; i < NUM_SENSORS; i++) {
+    EEPROM.write(EEPROM_SENSOR_IDS_ADDRESS + i, sensorIds[i]);
+  }
+  EEPROM.commit();
+}
 
 /**
  * ISR for storing the duration of gaps after pulses.
@@ -53,11 +101,45 @@ void IRAM_ATTR onReceiverChanged()
   }
 }
 
+void onButtonLongPressed()
+{
+  // Forget about which sensors are connected.
+  memset(sensorIds, EEPROM_EMPTY_VALUE, sizeof(sensorIds));
+  writeCurrentSensorsToStorage();
+  currentSensorIndex = 0;
+
+  Serial.println("Resetting known sensors.");
+}
+
 void setup()
 {
   Serial.begin(115200);
+
+  // On ESP32, this is needed since EEPROM is emulated through the flash storage, which in turn needs to know how many bytes this emulated storage should take up.
+  EEPROM.begin(NUM_SENSORS * sizeof(uint8_t));
+  // Write the empty value into sensor IDs so that writing the array back into EEPROM marks these entries as "empty".
+  memset(sensorIds, EEPROM_EMPTY_VALUE, NUM_SENSORS * sizeof(uint8_t));
+
+  Serial.println("Restoring sensor IDs from EEPROM: ");
+  for (currentSensorIndex = 0; currentSensorIndex < NUM_SENSORS; currentSensorIndex++)
+  {
+    uint8_t readId = EEPROM.read(EEPROM_SENSOR_IDS_ADDRESS + currentSensorIndex);
+    // If a special "empty" value was read, assume that this means there was no data (and not a sensor with an ID which randomly matched this value).
+    if (readId == EEPROM_EMPTY_VALUE)
+    {
+      break;
+    }
+    sensorIds[currentSensorIndex] = readId;
+    Serial.print("- ");
+    Serial.println(readId, HEX);
+  }
+  Serial.println("End of restoration list.");
+
   pinMode(RECEIVER_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(RECEIVER_PIN), onReceiverChanged, CHANGE);
+
+  button.setPressMs(1000);
+  button.attachLongPressStop(onButtonLongPressed);
 }
 
 bool isBetween(int32_t value, int32_t lower, int32_t upper)
@@ -65,7 +147,7 @@ bool isBetween(int32_t value, int32_t lower, int32_t upper)
   return lower <= value && value <= upper;
 }
 
-void printDecodedData(uint8_t *data)
+void handleData(uint8_t *data)
 {
   uint8_t id = data[0];
   uint8_t checksum = data[1] >> 4;
@@ -79,79 +161,120 @@ void printDecodedData(uint8_t *data)
   // Humidity is BCD-encoded with 2 decimal places, so multiply the first one by 10 to receive the decimal value.
   uint8_t humidity = (data[3] & 0x0F) * 10 + (data[4] >> 4);
 
-  char buf[256];
-  snprintf(buf, sizeof(buf), "ID: %02x\nTemp Celsius: %2.2f\nHumidity: %u%%\nChannel: %d\nIs button press: %d", id, temperatureCelsius, humidity, channelRaw, isButtonPress);
-  Serial.println(buf);
+  // If we were searching for sensors and the sensor had a button pressed on it to send this transmission, store the sensor's ID.
+  if (isSearchingForSensors())
+  {
+    if(isButtonPress && !isKnownSensor(id)) {
+      sensorIds[currentSensorIndex++] = id;
+      writeCurrentSensorsToStorage();
 
-  return;
+      Serial.print("Added sensor with ID ");
+      Serial.print(id, HEX);
+      Serial.println(" to known sensors.");
+    }
+  }
+  else {
+    if(isKnownSensor(id)) {
+      uint8_t *lastDataForThisSensor;
+      size_t sensorIndex;
+      for (sensorIndex = 0; sensorIndex < NUM_SENSORS; sensorIndex++)
+      {
+        if (sensorIds[sensorIndex] == id)
+        {
+          lastDataForThisSensor = &(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]);
+          break;
+        }
+      }
+
+      // Check whether received data differs from last received data (sensor sends same data multiple times for fault tolerance).
+      if (memcmp(lastDataForThisSensor, data, PAYLOAD_BYTES_COUNT) != 0)
+      {
+        // Store new data as last received data.
+        memcpy(&(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]), data, PAYLOAD_BYTES_COUNT);
+
+        char buf[256];
+        snprintf(buf, sizeof(buf), "ID: %02x\nTemp Celsius: %2.2f\nHumidity: %u%%\nChannel: %d\nIs button press: %d", id, temperatureCelsius, humidity, channelRaw, isButtonPress);
+        Serial.println(buf);
+      }
+      else {
+        Serial.println("Received data which is not different from previous data, ignoring.");
+      }
+    }
+    else {
+      Serial.print("Received data from sensor with unknown ID ");
+      Serial.print(id, HEX);
+      Serial.println(", ignoring it.");
+    }
+  }
 }
 
-void loop()
+void handleSignals()
 {
-
   for (size_t i = 0; i < currentSignalIndex; i++)
   {
     unsigned long currentGapDuration = gapDurations[i];
 
-    switch(currentState) {
-      case SEARCHING_PREFIX:
-        if(isBetween(currentGapDuration, SYNC_PREFIX_GAP_DURATION - TOLERANCE, SYNC_PREFIX_GAP_DURATION + TOLERANCE)) {
-          currentState = FOUND_PREFIX;
-        }
-        break;
-      case FOUND_PREFIX:
-        if(isBetween(currentGapDuration, SYNC_POSTFIX_GAP_DURATION - TOLERANCE, SYNC_POSTFIX_GAP_DURATION + TOLERANCE)) {
-          char buf[80];
-          snprintf(buf, sizeof(buf), "Read %d bytes of data.", dataBitIndex);
-          Serial.println(buf);
+    switch (currentState)
+    {
+    case SEARCHING_PREFIX:
+      if (isBetween(currentGapDuration, SYNC_PREFIX_GAP_DURATION - TOLERANCE, SYNC_PREFIX_GAP_DURATION + TOLERANCE))
+      {
+        currentState = FOUND_PREFIX;
+      }
+      break;
+    case FOUND_PREFIX:
+      if (isBetween(currentGapDuration, SYNC_POSTFIX_GAP_DURATION - TOLERANCE, SYNC_POSTFIX_GAP_DURATION + TOLERANCE))
+      {
+        char buf[80];
+        snprintf(buf, sizeof(buf), "Read %d bytes of data.", dataBitIndex);
+        //Serial.println(buf);
 
-          // Interpret data if we received the exact number of bits we expect and the last transmission
-          // hasn't been too recent (otherwise this will fire multiple times in a row since the sensor sends
-          // its data several times in a row).
-          if (dataBitIndex == PAYLOAD_BITS_COUNT && millis() - lastFullTransmissionMillis >= MIN_DELAY_BETWEEN_TRANSMISSIONS)
-          {
-            Serial.println("Data as hex: ");
-            for (size_t byteIndex = 0; byteIndex < PAYLOAD_BITS_COUNT / 8; byteIndex++)
-            {
-              Serial.print(dataBytes[byteIndex], HEX);
-              Serial.print(" ");
-            }
-            Serial.println();
-
-            printDecodedData(dataBytes);
-            lastFullTransmissionMillis = millis();
-          }
-
-          dataBitIndex = 0;
-          memset(dataBytes, false, sizeof(dataBytes));
-
-          currentState = SEARCHING_PREFIX;
-          break;
-        }
-
-        bool isHigh = isBetween(currentGapDuration, HIGH_GAP_DURATION - TOLERANCE, HIGH_GAP_DURATION + TOLERANCE);
-        bool isLow = isBetween(currentGapDuration, LOW_GAP_DURATION - TOLERANCE, LOW_GAP_DURATION + TOLERANCE);
-
-        // Reset received data if either:
-        // - Received data is not a valid bit duration
-        // - Transmission contains more bits than we expect
-        // If either happens, discard data currently in cache and go back to searching for the next transmission.
-        if (!isLow && !isHigh || dataBitIndex >= PAYLOAD_BITS_COUNT)
+        // Interpret data if we received the exact number of bits we expect and the last transmission
+        // hasn't been too recent (otherwise this will fire multiple times in a row since the sensor sends
+        // its data several times in a row).
+        if (dataBitIndex == PAYLOAD_BITS_COUNT)
         {
-          dataBitIndex = 0;
-          memset(dataBytes, false, sizeof(dataBytes));
-
-          currentState = SEARCHING_PREFIX;
-          break;
+          handleData(dataBytes);
         }
 
-        dataBytes[dataBitIndex / 8] |= isHigh << (7 - (dataBitIndex % 8));
-        dataBitIndex++;
+        dataBitIndex = 0;
+        memset(dataBytes, false, sizeof(dataBytes));
+
+        currentState = SEARCHING_PREFIX;
         break;
+      }
+
+      bool isHigh = isBetween(currentGapDuration, HIGH_GAP_DURATION - TOLERANCE, HIGH_GAP_DURATION + TOLERANCE);
+      bool isLow = isBetween(currentGapDuration, LOW_GAP_DURATION - TOLERANCE, LOW_GAP_DURATION + TOLERANCE);
+
+      // Reset received data if either:
+      // - Received data is not a valid bit duration
+      // - Transmission contains more bits than we expect
+      // If either happens, discard data currently in cache and go back to searching for the next transmission.
+      if (!isLow && !isHigh || dataBitIndex >= PAYLOAD_BITS_COUNT)
+      {
+        dataBitIndex = 0;
+        memset(dataBytes, false, sizeof(dataBytes));
+
+        currentState = SEARCHING_PREFIX;
+        break;
+      }
+
+      // Write received bit into the correct byte at the correct index, starting from the MSB.
+      dataBytes[dataBitIndex / 8] |= isHigh << (7 - (dataBitIndex % 8));
+      dataBitIndex++;
+      break;
     }
   }
 
   // Clear buffer with received gaps so that interrupt routine may fill the buffer again.
   currentSignalIndex = 0;
-  memset(gapDurations, 0, MAX_GAPS_COUNT);
+  memset(gapDurations, 0, sizeof(gapDurations));
+}
+
+void loop()
+{
+  button.tick();
+
+  handleSignals();
 }
