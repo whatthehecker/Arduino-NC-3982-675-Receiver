f96ef57e8fde931df419dbe376ff38a878f76ad7
Robert Hecker <robert.hecker@stud.h-da.de>
2024-09-05 00:03:44 +0200

    Clean up some code, discard MQTT implementation as using WiFi lags the ISR too much

--- src/main.cpp
+++ src/main.cpp
@@ -43,26 +43,120 @@ size_t currentSensorsCount = 0;
 size_t EEPROM_SENSOR_IDS_ADDRESS = 0x0;
 const uint8_t EEPROM_EMPTY_VALUE = 0xFF;
 
+struct SensorData
+{
+  uint8_t id;
+  uint8_t checksum;
+  bool isButtonPress;
+  float temperatureCelsius;
+  uint8_t channel;
+  uint8_t humidity;
+};
+
 // Stores the payload of the last transmission per sensor.
 // This could also be defined as a nested 2-dimensional array, but for simplicity sake this works too.
 uint8_t lastDataPerSensor[PAYLOAD_BYTES_COUNT * NUM_SENSORS];
 
-float lastPerceivedTemperaturesCelsius[NUM_SENSORS];
+float lastApparentTemperatures[NUM_SENSORS];
 
+size_t findSensorIndex(uint8_t sensorId);
 bool isKnownSensor(uint8_t sensorId);
 bool isSearchingForSensors();
 void writeCurrentSensorsToStorage();
+bool shouldCompareTemperatures();
+void updateTemperatureLed();
+void addSensor(uint8_t sensorId);
+float calculateApparentTemperature(float temperatureCelsius, float humidity);
+SensorData decodeData(uint8_t *data);
 
-bool isKnownSensor(uint8_t sensorId)
+SensorData decodeData(uint8_t *data)
+{
+  uint8_t id = data[0];
+  uint8_t checksum = data[1] >> 4;
+  uint16_t temperatureRaw = (data[2] << 4) | (data[3] >> 4);
+  uint8_t channelRaw = data[4] & 0x0F;
+  uint8_t isButtonPress = (data[1] >> 3) & 1;
+
+  float temperatureFahrenheit = (temperatureRaw - 900) * 0.1f;
+  float temperatureCelsius = (temperatureFahrenheit - 32.0) / 1.8;
+  // Humidity is BCD-encoded with 2 decimal places, so multiply the first one by 10 to receive the decimal value.
+  uint8_t humidity = (data[3] & 0x0F) * 10 + (data[4] >> 4);
+
+  return {
+      .id = id,
+      .checksum = checksum,
+      .isButtonPress = isButtonPress,
+      .temperatureCelsius = temperatureCelsius,
+      .channel = channelRaw,
+      .humidity = humidity,
+  };
+}
+
+float calculateApparentTemperature(float temperatureCelsius, float humidity)
+{
+  if (humidity >= 40 && temperatureCelsius >= 26.7)
+  {
+    // Formula and coefficients taken from https://en.wikipedia.org/wiki/Heat_index
+    // Empty comments added because VSCode likes to line wrap somewhere else without them.
+    return -8.784695                                                              //
+           + 1.61139411 * temperatureCelsius                                      //
+           + 2.338549 * humidity                                                  //
+           - 0.14611605 * temperatureCelsius * humidity                           //
+           - 1.2308094 * 1e-2 * temperatureCelsius * temperatureCelsius           //
+           - 1.6424828 * 1e-2 * ((float)humidity) * humidity                      //
+           + 2.211732 * 1e-3 * temperatureCelsius * temperatureCelsius * humidity //
+           + 7.2546 * 1e-4 * temperatureCelsius * ((float)humidity) * humidity    //
+           - 3.582 * 1e-6 * temperatureCelsius * temperatureCelsius * ((float)humidity) * humidity;
+  }
+
+  return temperatureCelsius;
+}
+
+void addSensor(uint8_t sensorId)
+{
+  sensorIds[currentSensorsCount++] = sensorId;
+  writeCurrentSensorsToStorage();
+
+  Serial.print("Added sensor with ID ");
+  Serial.print(sensorId, HEX);
+  Serial.println(" to known sensors.");
+}
+
+bool shouldCompareTemperatures()
+{
+  return NUM_SENSORS >= 2 && currentSensorsCount >= 2;
+}
+
+void updateTemperatureLed()
+{
+  float insidePerceivedTemp = lastApparentTemperatures[0];
+  float outsidePerceivedTemp = lastApparentTemperatures[1];
+
+  digitalWrite(LED_PIN, outsidePerceivedTemp < insidePerceivedTemp);
+
+  Serial.print("Perceived temperatures: ");
+  Serial.print(insidePerceivedTemp);
+  Serial.print(" deg Celsius inside / ");
+  Serial.print(outsidePerceivedTemp);
+  Serial.println(" deg Celsius outside");
+}
+
+// Returns the index of the sensor with the given ID in the array of sensor IDs or NUM_SENSORS if not found.
+size_t findSensorIndex(uint8_t sensorId)
 {
   for (size_t i = 0; i < currentSensorsCount; i++)
   {
     if (sensorIds[i] == sensorId)
     {
-      return true;
+      return i;
     }
   }
-  return false;
+  return NUM_SENSORS;
+}
+
+bool isKnownSensor(uint8_t sensorId)
+{
+  return findSensorIndex(sensorId) < NUM_SENSORS;
 }
 
 bool isSearchingForSensors()
@@ -91,18 +185,16 @@ void IRAM_ATTR onReceiverChanged()
     return;
   }
 
-  bool isNowHigh = digitalRead(RECEIVER_PIN) == HIGH;
-
+  bool isNowLow = digitalRead(RECEIVER_PIN) == LOW;
   // If this is the start of a gap, start measuring time.
-  if (!isNowHigh)
+  if (isNowLow)
   {
     gapStartMicros = micros();
   }
   // If this is the end of a gap (a start of a pulse), note down the elapsed time and store it.
   else
   {
-    unsigned long timeDiff = micros() - gapStartMicros;
-    gapDurations[currentSignalIndex++] = timeDiff;
+    gapDurations[currentSignalIndex++] = micros() - gapStartMicros;
   }
 }
 
@@ -140,8 +232,8 @@ void setup()
     Serial.println(readId, HEX);
   }
   Serial.print("Finished reading stored sensor IDs. Read ");
-  Serial.println(currentSensorsCount);
-  Serial.print(" sensors IDs.");
+  Serial.print(currentSensorsCount);
+  Serial.println(" sensors IDs.");
 
   pinMode(RECEIVER_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(RECEIVER_PIN), onReceiverChanged, CHANGE);
@@ -160,104 +252,47 @@ bool isBetween(int32_t value, int32_t lower, int32_t upper)
 
 void handleData(uint8_t *data)
 {
-  uint8_t id = data[0];
-  uint8_t checksum = data[1] >> 4;
-  // TODO: figure out why data[2] is being shifted although it is declared with full 8 bits of data.
-  uint16_t temperatureRaw = (data[2] << 4) | (data[3] >> 4);
-  uint8_t channelRaw = data[4] & 0x0F;
-  uint8_t isButtonPress = (data[1] >> 3) & 1;
-
-  float temperatureFahrenheit = (temperatureRaw - 900) * 0.1f;
-  float temperatureCelsius = (temperatureFahrenheit - 32.0) / 1.8;
-  // Humidity is BCD-encoded with 2 decimal places, so multiply the first one by 10 to receive the decimal value.
-  uint8_t humidity = (data[3] & 0x0F) * 10 + (data[4] >> 4);
-
-  float perceivedTemperature;
-  if (humidity >= 40 && temperatureCelsius >= 26.7)
-  {
-    // Formula and coefficients taken from https://en.wikipedia.org/wiki/Heat_index
-    // Empty comments added because VSCode likes to line wrap somewhere else without them.
-    perceivedTemperature =
-        -8.784695                                                               //
-        + 1.61139411 * temperatureCelsius                                       //
-        + 2.338549 * humidity                                                   //
-        - 0.14611605 * temperatureCelsius * humidity                            //
-        - 1.2308094 * 1e-2 * temperatureCelsius * temperatureCelsius           //
-        - 1.6424828 * 1e-2 * ((float)humidity) * humidity                      //
-        + 2.211732 * 1e-3 * temperatureCelsius * temperatureCelsius * humidity //
-        + 7.2546 * 1e-4 * temperatureCelsius * ((float)humidity) * humidity    //
-        - 3.582 * 1e-6 * temperatureCelsius * temperatureCelsius * ((float)humidity) * humidity;
-  }
-  else
-  {
-    perceivedTemperature = temperatureCelsius;
-  }
+  SensorData decoded = decodeData(data);
+  float apparentTemperature = calculateApparentTemperature(decoded.temperatureCelsius, decoded.humidity);
 
   // If we were searching for sensors and the sensor had a button pressed on it to send this transmission, store the sensor's ID.
-  if (isSearchingForSensors())
+  if (isSearchingForSensors() && !isKnownSensor(decoded.id) && decoded.isButtonPress)
   {
-    if (isButtonPress && !isKnownSensor(id))
-    {
-      sensorIds[currentSensorsCount++] = id;
-      writeCurrentSensorsToStorage();
-
-      Serial.print("Added sensor with ID ");
-      Serial.print(id, HEX);
-      Serial.println(" to known sensors.");
-    }
+    addSensor(decoded.id);
   }
-  else
+
+  // If sensor was not added during previous step, it is not one we are interested in.
+  if (!isKnownSensor(decoded.id))
   {
-    if (isKnownSensor(id))
-    {
-      uint8_t *lastDataForThisSensor;
-      size_t sensorIndex;
-      for (sensorIndex = 0; sensorIndex < NUM_SENSORS; sensorIndex++)
-      {
-        if (sensorIds[sensorIndex] == id)
-        {
-          lastDataForThisSensor = &(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]);
-          break;
-        }
-      }
+    Serial.print("Received data from sensor with unknown ID ");
+    Serial.print(decoded.id, HEX);
+    Serial.println(", ignoring it.");
+    return;
+  }
 
-      // Check whether received data differs from last received data (sensor sends same data multiple times for fault tolerance).
-      if (memcmp(lastDataForThisSensor, data, PAYLOAD_BYTES_COUNT) != 0)
-      {
-        // Store new data as last received data.
-        memcpy(&(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]), data, PAYLOAD_BYTES_COUNT);
-        lastPerceivedTemperaturesCelsius[sensorIndex] = perceivedTemperature;
+  size_t sensorIndex = findSensorIndex(decoded.id);
+  uint8_t *lastDataForThisSensor = &(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]);
 
-        char buf[256];
-        snprintf(buf, sizeof(buf),
-                 "ID: %02x, Temp Celsius: %2.2f (feels like %2.2f), Humidity: %u%%, Channel: %d, Is button press: %d",
-                 id, temperatureCelsius, perceivedTemperature, humidity, channelRaw, isButtonPress);
-        Serial.println(buf);
+  // If new data is same as old data, ignore.
+  if (memcmp(lastDataForThisSensor, data, PAYLOAD_BYTES_COUNT) == 0)
+  {
+    Serial.println("Received data which is not different from previous data, ignoring.");
+    return;
+  }
 
-        if(NUM_SENSORS >= 2 && currentSensorsCount >= 2) {
-          float insidePerceivedTemp = lastPerceivedTemperaturesCelsius[0];
-          float outsidePerceivedTemp = lastPerceivedTemperaturesCelsius[1];
+  // Store new data as last received data.
+  memcpy(&(lastDataPerSensor[sensorIndex * PAYLOAD_BYTES_COUNT]), data, PAYLOAD_BYTES_COUNT);
+  lastApparentTemperatures[sensorIndex] = apparentTemperature;
 
-          digitalWrite(LED_PIN, outsidePerceivedTemp < insidePerceivedTemp);
+  char buf[256];
+  snprintf(buf, sizeof(buf),
+           "ID: %02X, Temp Celsius: %2.2f (feels like %2.2f), Humidity: %u%%, Channel: %d, Is button press: %d",
+           decoded.id, decoded.temperatureCelsius, apparentTemperature, decoded.humidity, decoded.channel, decoded.isButtonPress);
+  Serial.println(buf);
 
-          Serial.print("Perceived temperatures: ");
-          Serial.print(insidePerceivedTemp);
-          Serial.print(" deg Celsius inside / ");
-          Serial.print(outsidePerceivedTemp);
-          Serial.println(" deg Celsius outside");
-        }
-      }
-      else
-      {
-        Serial.println("Received data which is not different from previous data, ignoring.");
-      }
-    }
-    else
-    {
-      Serial.print("Received data from sensor with unknown ID ");
-      Serial.print(id, HEX);
-      Serial.println(", ignoring it.");
-    }
+  if (shouldCompareTemperatures())
+  {
+    updateTemperatureLed();
   }
 }
 
@@ -324,6 +359,5 @@ void handleSignals()
 void loop()
 {
   button.tick();
-
   handleSignals();
 }
